b00a261972055efe23bc022646267be294820a9d
simoganger <simonsoft7@gmail.com>
2021-04-16 00:33:57 +0100

    Initial commit

---
+++ src/test/java/com/simontech/flux/courrier/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simontech.flux.courrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
